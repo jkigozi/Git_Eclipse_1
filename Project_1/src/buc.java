effa0e093ccb60e5f04ebfe2ed21c9cecfe360d0
John K <jkigozi@gmail.com>
2012-08-30 14:57:26 -0700

    Updated class buc.java
    Signed-off-by: John K <jkigozi@gmail.com>

--- Project_1/src/buc.java
+++ Project_1/src/buc.java
@@ -1,10 +1,22 @@
-
+import java.util.*;
+import java.io.*;
 public class buc {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws Exception{
+		InputStreamReader isr = new InputStreamReader(System.in); 
+		BufferedReader br = new BufferedReader(isr);
+		
+		System.out.print("What is your name:  ");
+		String nam = br.readLine();
+		
+		System.out.println("What is your dateofbirth:  ");
+		int dob = Integer.parseInt(br.readLine());
+				
+		System.out.print("What is currentyear:  ");
+		int curyr = Integer.parseInt(br.readLine());
+		
+		System.out.println("Welcome "+nam+". You are "+(curyr-dob));
 		 
 
 	}
